eadb00f1e7703f2ede3bf5f0579d4fdb86abf4a4
Tim Rothaemel <timrothamel@gmail.com>
2026-02-28 23:47:24 +0100

    added vite and index.html

--- src/scripts/supabase/initialize-supabase.js
+++ src/scripts/supabase/initialize-supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '../../../node_modules/@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
 const anonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRicnhydGJmeWx0eWZkbmxiY2Z5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMTA4NDcsImV4cCI6MjA4Nzc4Njg0N30.4Q-EHWFm4TQ-wwpAM7FAHieqDKC2AluuUSpp9Mkb0uc"
 const url = "https://dbrxrtbfyltyfdnlbcfy.supabase.co"
