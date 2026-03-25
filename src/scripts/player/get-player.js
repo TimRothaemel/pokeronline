753aaed05a1b2a5e4d6c93eb57d58dae50120d40
Tim Rothaemel <timrothamel@gmail.com>
2026-03-25 17:52:10 +0100

    feat: add function to get user data from DB

---
+++ src/scripts/player/get-player.js
@@ -0,0 +1,16 @@
+import supabase from "../supabase/initialize-supabase";
+
+export async function getPlayer(playerId) {
+  const { data, error } = await supabase
+    .from("players")
+    .select("*")
+    .eq("id", playerId)
+    .single();
+
+  if (error) {
+    console.error("Error fetching player:", error);
+    return null;
+  }
+
+  return data;
+}
