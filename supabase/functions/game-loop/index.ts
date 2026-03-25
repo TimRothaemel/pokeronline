6738027b3d8cd3541d11976ef83b730d6f7c1776
Tim Rothaemel <timrothamel@gmail.com>
2026-03-25 17:38:11 +0100

    feat: game-loop backend response now and call the right functions

--- supabase/functions/game-loop/index.ts
+++ supabase/functions/game-loop/index.ts
@@ -17,3 +17,42 @@ async function notifyPlayer(roomId: string, playerId: string, message: string, a
   if (error) throw error;
   return data;
 }
+
+Deno.serve(async (req: Request) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
+  const { roomId, playerId} = await req.json();
+  if (!roomId || !playerId) {
+    return new Response(
+      JSON.stringify({ error: "roomId, playerId and message are required" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+    let firstPlayer = await getFirstPlayer(roomId)
+    await notifyPlayer(roomId, firstPlayer.id, "small_blind", 25);
+
+  if (error) {// throw error response
+    return new Response(
+      JSON.stringify({ error: "Game loop failed" }),
+      {
+        status: 404,
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+      },
+    );
+  }
+  return new Response(// throw success response
+    JSON.stringify({
+      success: true,
+      message: "Game loop executed successfully",
+    }),
+    {
+      status: 200,
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+    },
+  );
+});
